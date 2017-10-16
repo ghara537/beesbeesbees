25c2035406bc0fec4d460e00493789df9b97c46f
Garrett Hara <ghara537@gmail.com>
2017-10-16 14:26:10 -0700

    Implementation of all Bees v1

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  // new Grub.call(this);
+  // this.age = 5;
+  // this.color = 'yellow';
+  // this.job = 'keep on growing';
 };
 
+// Bee.prototype = Grub.prototype;
+// Bee.prototype.constructor = Bee;
+
+
+//Bee.prototype.job = 'keep on '
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+  new ForageBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.forage = 'I am too old, let me play cards instead'
 };
+
+RetiredForagerBee.prototype = Object.create(ForageBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+}
 
